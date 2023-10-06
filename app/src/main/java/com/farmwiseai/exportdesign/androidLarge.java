3c39fd42dc6d181f24724df590a5351bbd7bfa23
Shriya75 <shriya@farmwiseai.com>
2023-10-06 10:47:21 +0530

    Commit

---
+++ app/src/main/java/com/farmwiseai/exportdesign/androidLarge.java
@@ -0,0 +1,62 @@
+package com.farmwiseai.exportdesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class androidLarge extends Activity {
+
+	
+	private View _bg__android_large___1_ek2;
+	private View _bg__bg_img1_1_ek1;
+	private ImageView mask_group;
+	private View _bg__frame_4_ek1;
+	private View _bg__frame_5_ek1;
+	private View _bg__rectangle_2;
+	private View rectangle_2_ek4;
+	private View _bg__rectangle_3;
+	private View _bg__rectangle_2_ek6;
+	private View rectangle_2_ek7;
+	private View _bg__rectangle_4;
+	private View _bg__rectangle_4_ek5;
+	private View rectangle_2_ek8;
+	private View _bg__frame_2_ek1;
+	private ImageView frame_3;
+
+
+	private TextView we_understand_being_a_farmer_is_not_easy;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_androidlarge);
+
+		
+		_bg__android_large___1_ek2 = (View) findViewById(R.id._bg__android_large___1_ek2);
+		_bg__bg_img1_1_ek1 = (View) findViewById(R.id._bg__bg_img1_1_ek1);
+		mask_group = (ImageView) findViewById(R.id.mask_group);
+
+		_bg__rectangle_2 = (View) findViewById(R.id._bg__rectangle_2);
+		rectangle_2_ek4 = (View) findViewById(R.id.rectangle_2_ek4);
+		_bg__rectangle_3 = (View) findViewById(R.id._bg__rectangle_3);
+		_bg__rectangle_2_ek6 = (View) findViewById(R.id._bg__rectangle_2_ek6);
+		rectangle_2_ek7 = (View) findViewById(R.id.rectangle_2_ek7);
+		_bg__rectangle_4 = (View) findViewById(R.id._bg__rectangle_4);
+		_bg__rectangle_4_ek5 = (View) findViewById(R.id._bg__rectangle_4_ek5);
+		rectangle_2_ek8 = (View) findViewById(R.id.rectangle_2_ek8);
+		_bg__frame_2_ek1 = (View) findViewById(R.id._bg__frame_2_ek1);
+		frame_3 = (ImageView) findViewById(R.id.frame_3);
+		we_understand_being_a_farmer_is_not_easy = (TextView) findViewById(R.id.we_understand_being_a_farmer_is_not_easy);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
